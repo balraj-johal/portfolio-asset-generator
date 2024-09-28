362bb99c967fc655246ff5624208e4152a1b262f
Balraj <balraj.johal@phntms.com>
2024-09-28 22:38:11 +0100

    Init remotion project

---
+++ src/Root.tsx
@@ -0,0 +1,19 @@
+import { Composition } from "remotion";
+import { MINUTE_IN_FRAMES } from "./config";
+
+import "./styles/index.css";
+
+export const RemotionRoot = () => {
+  return (
+    <>
+      {/* <Composition
+        id="MenuOne"
+        component={MenuOne}
+        durationInFrames={MINUTE_IN_FRAMES * 30}
+        fps={24}
+        width={1280}
+        height={720}
+      /> */}
+    </>
+  );
+};
